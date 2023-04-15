03032c2ac4cce21268fa9c2d4fe0535aa6f8270a
tunahanoguz <tunahanog@gmail.com>
2023-04-15 16:36:47 +0300

    Create BookReviewLike entity

---
+++ Core/SocialBook.Domain/Books/BookReviewLike.cs
@@ -0,0 +1,18 @@
+﻿using SocialBook.Domain.Common;
+using SocialBook.Domain.Identity;
+
+namespace SocialBook.Domain.Books
+{
+    public class BookReviewLike : BaseEntity
+    {
+        /// <summary>
+        /// Gets or sets the book review
+        /// </summary>
+        public BookReview BookReview { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user who liked the book review
+        /// </summary>
+        public AppUser User { get; set; }
+    }
+}
