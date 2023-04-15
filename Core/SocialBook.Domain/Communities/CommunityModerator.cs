6c6d5a4b55f7c329ed61df22f94f29cd4aacf7be
tunahanoguz <tunahanog@gmail.com>
2023-04-15 20:43:37 +0300

    Add summaries of properties to community moderator entity

--- Core/SocialBook.Domain/Communities/CommunityModerator.cs
+++ Core/SocialBook.Domain/Communities/CommunityModerator.cs
@@ -5,7 +5,14 @@ namespace SocialBook.Domain.Communities
 {
     public class CommunityModerator : BaseEntity
     {
+        /// <summary>
+        /// Gets or sets the community the user is a moderator of
+        /// </summary>
         public Community Community { get; set; }
+
+        /// <summary>
+        /// Gets or sets the moderator user
+        /// </summary>
         public AppUser User { get; set; }
     }
 }
