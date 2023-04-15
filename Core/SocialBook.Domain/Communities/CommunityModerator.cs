30f1058ba2bd0dac8b784880cf4838b2cd129373
tunahanoguz <tunahanog@gmail.com>
2023-04-15 19:37:11 +0300

    Create community moderator entity

---
+++ Core/SocialBook.Domain/Communities/CommunityModerator.cs
@@ -0,0 +1,11 @@
+﻿using SocialBook.Domain.Common;
+using SocialBook.Domain.Identity;
+
+namespace SocialBook.Domain.Communities
+{
+    public class CommunityModerator : BaseEntity
+    {
+        public Community Community { get; set; }
+        public AppUser User { get; set; }
+    }
+}
