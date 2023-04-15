87cbe0af48d61c64083f3ff11dd743c189f2c850
tunahanoguz <tunahanog@gmail.com>
2023-04-15 19:38:36 +0300

    Create community post entity

---
+++ Core/SocialBook.Domain/Communities/CommunityPost.cs
@@ -0,0 +1,13 @@
+﻿using SocialBook.Domain.Common;
+using SocialBook.Domain.Identity;
+
+namespace SocialBook.Domain.Communities
+{
+    public class CommunityPost : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public Community Community { get; set; }
+        public AppUser User { get; set; }
+    }
+}
