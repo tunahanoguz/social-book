0d7705ffaf2c029e53aa467e93baf4e52566dcce
tunahanoguz <tunahanog@gmail.com>
2023-04-15 19:35:42 +0300

    Create community member entity

---
+++ Core/SocialBook.Domain/Communities/CommunityMember.cs
@@ -0,0 +1,11 @@
+﻿using SocialBook.Domain.Common;
+using SocialBook.Domain.Identity;
+
+namespace SocialBook.Domain.Communities
+{
+    public class CommunityMember : BaseEntity
+    {
+        public Community Community { get; set; }
+        public AppUser User { get; set; }
+    }
+}
