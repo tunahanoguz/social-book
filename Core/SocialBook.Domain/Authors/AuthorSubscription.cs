6aa6c0d577628dc01610826d25f3cd4f1e7e583e
tunahanoguz <tunahanog@gmail.com>
2023-04-15 19:17:24 +0300

    Create author subscription entity

---
+++ Core/SocialBook.Domain/Authors/AuthorSubscription.cs
@@ -0,0 +1,18 @@
+﻿using SocialBook.Domain.Common;
+using SocialBook.Domain.Identity;
+
+namespace SocialBook.Domain.Authors
+{
+    public class AuthorSubscription : BaseEntity
+    {
+        /// <summary>
+        /// Gets or sets the subscribed author
+        /// </summary>
+        public Author Author { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user who subscribed to the author
+        /// </summary>
+        public AppUser User { get; set; }
+    }
+}
