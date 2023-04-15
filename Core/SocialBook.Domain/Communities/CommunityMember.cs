1ae8228b1f0bce0d1e1b974de2e53d6582737395
tunahanoguz <tunahanog@gmail.com>
2023-04-15 20:42:31 +0300

    Add summaries of properties to community member entity

--- Core/SocialBook.Domain/Communities/CommunityMember.cs
+++ Core/SocialBook.Domain/Communities/CommunityMember.cs
@@ -5,7 +5,14 @@ namespace SocialBook.Domain.Communities
 {
     public class CommunityMember : BaseEntity
     {
+        /// <summary>
+        /// Gets or sets the community the user is a member of
+        /// </summary>
         public Community Community { get; set; }
+
+        /// <summary>
+        /// Gets or sets the member user
+        /// </summary>
         public AppUser User { get; set; }
     }
 }
