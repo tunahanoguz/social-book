1c09c7a593ce70268a2caebe8e52ce51cb342c74
tunahanoguz <tunahanog@gmail.com>
2023-04-15 20:39:28 +0300

    Add summaries of properties to community post entity

--- Core/SocialBook.Domain/Communities/CommunityPost.cs
+++ Core/SocialBook.Domain/Communities/CommunityPost.cs
@@ -5,9 +5,24 @@ namespace SocialBook.Domain.Communities
 {
     public class CommunityPost : BaseEntity
     {
+        /// <summary>
+        /// Gets or sets the title
+        /// </summary>
         public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the body
+        /// </summary>
         public string Body { get; set; }
+
+        /// <summary>
+        /// Gets or sets the community that the post was published
+        /// </summary>
         public Community Community { get; set; }
+
+        /// <summary>
+        /// Gets or sets the post owner user
+        /// </summary>
         public AppUser User { get; set; }
     }
 }
