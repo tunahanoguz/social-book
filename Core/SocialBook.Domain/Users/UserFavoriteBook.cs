ab568d07e0500f4f16d057ae0b8b4a33bf8645b6
tunahanoguz <tunahanog@gmail.com>
2023-04-15 21:07:31 +0300

    Create user favorite book entity

---
+++ Core/SocialBook.Domain/Users/UserFavoriteBook.cs
@@ -0,0 +1,19 @@
+﻿using SocialBook.Domain.Books;
+using SocialBook.Domain.Common;
+using SocialBook.Domain.Identity;
+
+namespace SocialBook.Domain.Users
+{
+    public class UserFavoriteBook : BaseEntity
+    {
+        /// <summary>
+        /// Gets or sets the user
+        /// </summary>
+        public AppUser User { get; set; }
+
+        /// <summary>
+        /// Gets or sets the book
+        /// </summary>
+        public Book Book { get; set; }
+    }
+}
