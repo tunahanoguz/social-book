c83edaada65385aa784ab6fd1cbb65aa6ff167c9
tunahanoguz <tunahanog@gmail.com>
2023-04-15 19:34:06 +0300

    Create community genre entity

---
+++ Core/SocialBook.Domain/Communities/CommunityGenre.cs
@@ -0,0 +1,17 @@
+﻿using SocialBook.Domain.Common;
+
+namespace SocialBook.Domain.Communities
+{
+    public class CommunityGenre : BaseEntity
+    {
+        /// <summary>
+        /// Gets or sets the community
+        /// </summary>
+        public Community Community { get; set; }
+
+        /// <summary>
+        /// Gets or sets the community genre
+        /// </summary>
+        public Genre Genre { get; set; }
+    }
+}
