a31647e45b6b662aac7993b652e0225c9b9e9fa4
tunahanoguz <tunahanog@gmail.com>
2023-04-15 11:56:04 +0300

    Create identity entities

---
+++ Core/SocialBook.Domain/Identity/AppUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SocialBook.Domain.Identity
+{
+    public class AppUser: IdentityUser<string>
+    {
+        /// <summary>
+        /// Gets or sets the first name
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last name
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
