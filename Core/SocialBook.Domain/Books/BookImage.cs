30f545d870bb55479ecb014526d5e720c06736d6
tunahanoguz <tunahanog@gmail.com>
2023-04-15 16:27:55 +0300

    Create book image entity

---
+++ Core/SocialBook.Domain/Books/BookImage.cs
@@ -0,0 +1,27 @@
+﻿using SocialBook.Domain.Common;
+
+namespace SocialBook.Domain.Books
+{
+    public class BookImage : BaseEntity
+    {
+        /// <summary>
+        /// Gets or sets the image file name
+        /// </summary>
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the image file extension
+        /// </summary>
+        public string FileExtension { get; set; }
+
+        /// <summary>
+        /// Gets or sets the image file size
+        /// </summary>
+        public double FileSize { get; set; }
+
+        /// <summary>
+        /// Gets or sets the associated book
+        /// </summary>
+        public Book Book { get; set; }
+    }
+}
