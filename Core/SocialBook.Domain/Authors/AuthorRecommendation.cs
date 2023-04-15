0e32ec29e256705618a58dae48ec26bf0817aa3c
tunahanoguz <tunahanog@gmail.com>
2023-04-15 19:15:58 +0300

    Create author recommendation entity

---
+++ Core/SocialBook.Domain/Authors/AuthorRecommendation.cs
@@ -0,0 +1,23 @@
+﻿using SocialBook.Domain.Common;
+using SocialBook.Domain.Identity;
+
+namespace SocialBook.Domain.Authors
+{
+    public class AuthorRecommendation : BaseEntity
+    {
+        /// <summary>
+        /// Gets or sets the recommended author
+        /// </summary>
+        public Author Author { get; set; }
+
+        /// <summary>
+        /// Gets or sets the recommender user
+        /// </summary>
+        public AppUser RecommenderUser { get; set; }
+
+        /// <summary>
+        /// Gets or sets the recommendation recipient user
+        /// </summary>
+        public AppUser RecipientUser { get; set; }
+    }
+}
