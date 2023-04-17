d60b69f4e4b43fd5e19f9280784760cd53d98a2a
tunahanoguz <tunahanog@gmail.com>
2023-04-17 20:43:15 +0300

    Create read and write repositories for CommunityPost entity

---
+++ Infrastructure/SocialBook.Persistence/Repositories/Communities/CommunityPost/CommunityPostWriteRepository.cs
@@ -0,0 +1,14 @@
+﻿using SocialBook.Application.Repositories.Communities;
+using SocialBook.Domain.Entities.Communities;
+using SocialBook.Persistence.Contexts;
+using SocialBook.Persistence.Repositories.Common;
+
+namespace SocialBook.Persistence.Repositories.Communities
+{
+    public class CommunityPostWriteRepository : WriteRepository<CommunityPost>, ICommunityPostWriteRepository
+    {
+        public CommunityPostWriteRepository(SocialBookAPIDbContext context) : base(context)
+        {
+        }
+    }
+}
