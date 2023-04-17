f66f5ff9f329f5965fbbcdbbbaa8b40fe0c630a3
tunahanoguz <tunahanog@gmail.com>
2023-04-17 14:22:26 +0300

    Create read and write repositories interfaces for UserSocialMedia entity

---
+++ Core/SocialBook.Application/Repositories/Users/UserSocialMedia/IUserSocialMediaReadRepository.cs
@@ -0,0 +1,9 @@
+﻿using SocialBook.Application.Repositories.Common;
+using SocialBook.Domain.Users;
+
+namespace SocialBook.Application.Repositories.Users
+{
+    public interface IUserSocialMediaReadRepository : IReadRepository<UserSocialMedia>
+    {
+    }
+}
