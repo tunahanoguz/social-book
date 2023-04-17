3f8de1c25de2f71e05fd59540f31d5c1265d48ec
tunahanoguz <tunahanog@gmail.com>
2023-04-17 15:27:38 +0300

    Create read and write repositories for genre entity

---
+++ Infrastructure/SocialBook.Persistence/Repositories/Common/Genre/GenreWriteRepository.cs
@@ -0,0 +1,13 @@
+﻿using SocialBook.Application.Repositories.Common;
+using SocialBook.Domain.Common;
+using SocialBook.Persistence.Contexts;
+
+namespace SocialBook.Persistence.Repositories.Common
+{
+    public class GenreWriteRepository : WriteRepository<Genre>, IGenreWriteRepository
+    {
+        public GenreWriteRepository(SocialBookAPIDbContext context) : base(context)
+        {
+        }
+    }
+}
