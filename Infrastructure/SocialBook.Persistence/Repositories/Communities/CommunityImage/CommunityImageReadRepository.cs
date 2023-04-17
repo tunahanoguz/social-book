864030aff0f29f994476c48b5bb5eeeeea8489ba
tunahanoguz <tunahanog@gmail.com>
2023-04-17 20:37:11 +0300

    Create read and write repositories for CommunityImage entity

---
+++ Infrastructure/SocialBook.Persistence/Repositories/Communities/CommunityImage/CommunityImageReadRepository.cs
@@ -0,0 +1,14 @@
+﻿using SocialBook.Application.Repositories.Communities;
+using SocialBook.Domain.Entities.Communities;
+using SocialBook.Persistence.Contexts;
+using SocialBook.Persistence.Repositories.Common;
+
+namespace SocialBook.Persistence.Repositories.Communities
+{
+    public class CommunityImageReadRepository : ReadRepository<CommunityImage>, ICommunityImageReadRepository
+    {
+        public CommunityImageReadRepository(SocialBookAPIDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ Infrastructure/SocialBook.Persistence/Repositories/Communities/CommunityImage/CommunityImageWriteRepository.cs
@@ -0,0 +1,14 @@
+﻿using SocialBook.Application.Repositories.Communities;
+using SocialBook.Domain.Entities.Communities;
+using SocialBook.Persistence.Contexts;
+using SocialBook.Persistence.Repositories.Common;
+
+namespace SocialBook.Persistence.Repositories.Communities
+{
+    public class CommunityImageWriteRepository : WriteRepository<CommunityImage>, ICommunityImageWriteRepository
+    {
+        public CommunityImageWriteRepository(SocialBookAPIDbContext context) : base(context)
+        {
+        }
+    }
+}
