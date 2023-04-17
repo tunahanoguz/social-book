a5efa38a9e6b8d3b70af986c6bc5b912f919ee5c
tunahanoguz <tunahanog@gmail.com>
2023-04-17 20:28:53 +0300

    Create read and write repositories for AuthorReviewImage entity

---
+++ Infrastructure/SocialBook.Persistence/Repositories/Authors/AuthorReviewImage/AuthorReviewImageReadRepository.cs
@@ -0,0 +1,14 @@
+﻿using SocialBook.Application.Repositories.Authors;
+using SocialBook.Domain.Entities.Authors;
+using SocialBook.Persistence.Contexts;
+using SocialBook.Persistence.Repositories.Common;
+
+namespace SocialBook.Persistence.Repositories.Authors
+{
+    public class AuthorReviewImageReadRepository : ReadRepository<AuthorReviewImage>, IAuthorReviewImageReadRepository
+    {
+        public AuthorReviewImageReadRepository(SocialBookAPIDbContext context) : base(context)
+        {
+        }
+    }
+}
