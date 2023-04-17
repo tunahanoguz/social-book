2dbffa873bf2c9556529bfbc4c5c60de75109dcb
tunahanoguz <tunahanog@gmail.com>
2023-04-17 16:34:30 +0300

    Create read and write repositories for AuthorGenre entity

---
+++ Infrastructure/SocialBook.Persistence/Repositories/Authors/AuthorGenre/AuthorGenreReadRepository.cs
@@ -0,0 +1,14 @@
+﻿using SocialBook.Application.Repositories.Authors;
+using SocialBook.Domain.Entities.Authors;
+using SocialBook.Persistence.Contexts;
+using SocialBook.Persistence.Repositories.Common;
+
+namespace SocialBook.Persistence.Repositories.Authors
+{
+    public class AuthorGenreReadRepository : ReadRepository<AuthorGenre>, IAuthorGenreReadRepository
+    {
+        public AuthorGenreReadRepository(SocialBookAPIDbContext context) : base(context)
+        {
+        }
+    }
+}
