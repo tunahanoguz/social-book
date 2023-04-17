bd1fbdfce5d6eeb5a54a93a07f39369597b66637
tunahanoguz <tunahanog@gmail.com>
2023-04-17 20:26:47 +0300

    Create read and write repositories for AuthorReviewDislike entity

---
+++ Infrastructure/SocialBook.Persistence/Repositories/Authors/AuthorReviewDislike/AuthorReviewDislikeReadRepository.cs
@@ -0,0 +1,14 @@
+﻿using SocialBook.Application.Repositories.Authors;
+using SocialBook.Domain.Entities.Authors;
+using SocialBook.Persistence.Contexts;
+using SocialBook.Persistence.Repositories.Common;
+
+namespace SocialBook.Persistence.Repositories.Authors
+{
+    public class AuthorReviewDislikeReadRepository : ReadRepository<AuthorReviewDislike>, IAuthorReviewDislikeReadRepository
+    {
+        public AuthorReviewDislikeReadRepository(SocialBookAPIDbContext context) : base(context)
+        {
+        }
+    }
+}
