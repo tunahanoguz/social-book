493570e31974a9045fd0eb0e394c2ba9334cea1a
tunahanoguz <tunahanog@gmail.com>
2023-04-17 10:40:57 +0300

    Create read and write repositories interfaces for BookReviewDislike entity

---
+++ Core/SocialBook.Application/Repositories/Books/BookReviewDislike/IBookReviewDislikeWriteRepository.cs
@@ -0,0 +1,9 @@
+﻿using SocialBook.Application.Repositories.Common;
+using SocialBook.Domain.Books;
+
+namespace SocialBook.Application.Repositories.Books
+{
+    public interface IBookReviewDislikeWriteRepository : IWriteRepository<BookReviewDislike>
+    {
+    }
+}
