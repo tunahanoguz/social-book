2dad9d61964309175e58c8b008576f79bdd3e8fb
tunahanoguz <tunahanog@gmail.com>
2023-04-17 20:59:33 +0300

    Create read and write repositories UserSocialMedia entities

---
+++ Infrastructure/SocialBook.Persistence/Repositories/Users/UserSocialMedia/UserSocialMediaReadRepository.cs
@@ -0,0 +1,14 @@
+﻿using SocialBook.Application.Repositories.Users;
+using SocialBook.Domain.Entities.Users;
+using SocialBook.Persistence.Contexts;
+using SocialBook.Persistence.Repositories.Common;
+
+namespace SocialBook.Persistence.Repositories.Users
+{
+    public class UserSocialMediaReadRepository : ReadRepository<UserSocialMedia>, IUserSocialMediaReadRepository
+    {
+        public UserSocialMediaReadRepository(SocialBookAPIDbContext context) : base(context)
+        {
+        }
+    }
+}
