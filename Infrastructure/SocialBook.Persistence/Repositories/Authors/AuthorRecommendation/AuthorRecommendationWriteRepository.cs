e9a0a79329a30749507862e977ecc000bbe96117
tunahanoguz <tunahanog@gmail.com>
2023-04-17 16:38:18 +0300

    Create read and write repositories for AuthorRecommendation entity

---
+++ Infrastructure/SocialBook.Persistence/Repositories/Authors/AuthorRecommendation/AuthorRecommendationWriteRepository.cs
@@ -0,0 +1,14 @@
+﻿using SocialBook.Application.Repositories.Authors;
+using SocialBook.Domain.Entities.Authors;
+using SocialBook.Persistence.Contexts;
+using SocialBook.Persistence.Repositories.Common;
+
+namespace SocialBook.Persistence.Repositories.Authors
+{
+    public class AuthorRecommendationWriteRepository : WriteRepository<AuthorRecommendation>, IAuthorRecommendationWriteRepository
+    {
+        public AuthorRecommendationWriteRepository(SocialBookAPIDbContext context) : base(context)
+        {
+        }
+    }
+}
