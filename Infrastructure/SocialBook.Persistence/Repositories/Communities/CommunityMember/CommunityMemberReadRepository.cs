f3c520ba3fd06749f8f2ec873af90845cd2f723a
tunahanoguz <tunahanog@gmail.com>
2023-04-17 20:38:59 +0300

    Create read and write repositories for CommunityMember entity

---
+++ Infrastructure/SocialBook.Persistence/Repositories/Communities/CommunityMember/CommunityMemberReadRepository.cs
@@ -0,0 +1,14 @@
+﻿using SocialBook.Application.Repositories.Communities;
+using SocialBook.Domain.Entities.Communities;
+using SocialBook.Persistence.Contexts;
+using SocialBook.Persistence.Repositories.Common;
+
+namespace SocialBook.Persistence.Repositories.Communities
+{
+    public class CommunityMemberReadRepository : ReadRepository<CommunityMember>, ICommunityMemberReadRepository
+    {
+        public CommunityMemberReadRepository(SocialBookAPIDbContext context) : base(context)
+        {
+        }
+    }
+}
