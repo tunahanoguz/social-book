17f264ad9b7f2070acd6e8b018e4cb3556fbf807
tunahanoguz <tunahanog@gmail.com>
2023-04-17 15:29:47 +0300

    Create read and write repositories for SiteSetting entity

---
+++ Infrastructure/SocialBook.Persistence/Repositories/Common/SiteSetting/SiteSettingReadRepository.cs
@@ -0,0 +1,13 @@
+﻿using SocialBook.Application.Repositories.Common;
+using SocialBook.Domain.Common;
+using SocialBook.Persistence.Contexts;
+
+namespace SocialBook.Persistence.Repositories.Common
+{
+    public class SiteSettingReadRepository : ReadRepository<SiteSetting>, ISiteSettingReadRepository
+    {
+        public SiteSettingReadRepository(SocialBookAPIDbContext context) : base(context)
+        {
+        }
+    }
+}
