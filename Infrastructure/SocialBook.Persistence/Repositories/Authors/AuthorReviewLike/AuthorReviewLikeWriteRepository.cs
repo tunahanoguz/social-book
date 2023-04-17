4f4588a8cbd0ffd6fb42c20ed3302289e244a4f6
tunahanoguz <tunahanog@gmail.com>
2023-04-17 20:24:42 +0300

    Create read and write repositories for AuthorReviewLike entity

---
+++ Infrastructure/SocialBook.Persistence/Repositories/Authors/AuthorReviewLike/AuthorReviewLikeWriteRepository.cs
@@ -0,0 +1,14 @@
+﻿using SocialBook.Application.Repositories.Authors;
+using SocialBook.Domain.Entities.Authors;
+using SocialBook.Persistence.Contexts;
+using SocialBook.Persistence.Repositories.Common;
+
+namespace SocialBook.Persistence.Repositories.Authors
+{
+    public class AuthorReviewLikeWriteRepository : WriteRepository<AuthorReviewLike>, IAuthorReviewLikeWriteRepository
+    {
+        public AuthorReviewLikeWriteRepository(SocialBookAPIDbContext context) : base(context)
+        {
+        }
+    }
+}
