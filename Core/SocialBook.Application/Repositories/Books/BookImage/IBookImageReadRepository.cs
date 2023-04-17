949b74a2b1a1325485070d899d2cbcfe0a8cacae
tunahanoguz <tunahanog@gmail.com>
2023-04-17 10:32:50 +0300

    Create book image read and write repositories interfaces

---
+++ Core/SocialBook.Application/Repositories/Books/BookImage/IBookImageReadRepository.cs
@@ -0,0 +1,9 @@
+﻿using SocialBook.Application.Repositories.Common;
+using SocialBook.Domain.Books;
+
+namespace SocialBook.Application.Repositories.Books
+{
+    internal interface IBookImageReadRepository : IReadRepository<BookImage>
+    {
+    }
+}
