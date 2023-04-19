e90645d08b40f4aeb22262fc63817a0348dfe229
tunahanoguz <tunahanog@gmail.com>
2023-04-19 17:59:01 +0300

    Rename AuthorReviewLike data seeder

--- Infrastructure/SocialBook.Persistence/DataSeeders/Authors/AuthorReviewLikeSeeder.cs
+++ Infrastructure/SocialBook.Persistence/DataSeeders/Authors/AuthorReviewLikeSeeder.cs
@@ -8,11 +8,9 @@ namespace SocialBook.Persistence.DataSeeders
         private static AuthorReviewLike[] AuthorReviewLikes = new AuthorReviewLike[]
         {
             new AuthorReviewLike { Id = new Guid(), AuthorReview = AuthorReviews[0], User = Users[1] },
-            new AuthorReviewLike { Id = new Guid(), AuthorReview = AuthorReviews[0], User = Users[1] },
-            new AuthorReviewLike { Id = new Guid(), AuthorReview = AuthorReviews[0], User = Users[1] },
-            new AuthorReviewLike { Id = new Guid(), AuthorReview = AuthorReviews[1], User = Users[0] },
-            new AuthorReviewLike { Id = new Guid(), AuthorReview = AuthorReviews[1], User = Users[0] },
-            new AuthorReviewLike { Id = new Guid(), AuthorReview = AuthorReviews[1], User = Users[0] },
+            new AuthorReviewLike { Id = new Guid(), AuthorReview = AuthorReviews[1], User = Users[1] },
+            new AuthorReviewLike { Id = new Guid(), AuthorReview = AuthorReviews[2], User = Users[0] },
+            new AuthorReviewLike { Id = new Guid(), AuthorReview = AuthorReviews[3], User = Users[0] },
         };
 
         public static void SeedAuthorReviewLikes(this ModelBuilder builder)
