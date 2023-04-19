43ad78348d6b754107ab3ab0a2cb0cc449c1c226
tunahanoguz <tunahanog@gmail.com>
2023-04-19 15:52:13 +0300

    Create data seeding for Author entity

--- Infrastructure/SocialBook.Persistence/Contexts/SocialBookAPIDbContext.cs
+++ Infrastructure/SocialBook.Persistence/Contexts/SocialBookAPIDbContext.cs
@@ -113,6 +113,38 @@ namespace SocialBook.Persistence.Contexts
             };
 
             builder.Entity<AppUserRole>().HasData(usersRoles);
+
+            var authors = new Author[]
+            {
+                new Author
+                {
+                    Id = new Guid(),
+                    FirstName = "Charles",
+                    LastName = "Dickens",
+                    CountryOfBirth = "United Kingdom",
+                    DateOfBirth = new DateTime(1812, 2, 7),
+                    MetaTitle = "Charles Dickens",
+                    MetaDescription = "Meta description for Charles Dickens",
+                    MetaKeywords = "charles dickens, charles dickens author, charles dickens books",
+                    Slug = "charles-dickens",
+                    CreatorUser = users[0]
+                },
+                new Author
+                {
+                    Id = new Guid(),
+                    FirstName = "Alexander",
+                    LastName = "Pushkin",
+                    CountryOfBirth = "Russia",
+                    DateOfBirth = new DateTime(1799, 6, 6),
+                    MetaTitle = "Alexander Pushkin",
+                    MetaDescription = "Meta description for Alexander Pushkin",
+                    MetaKeywords = "alexander pushkin, alexander pushkin author, alexander pushkin books",
+                    Slug = "alexander-pushkin",
+                    CreatorUser = users[1]
+                }
+            };
+
+            builder.Entity<Author>().HasData(authors);
         }
     }
 }
