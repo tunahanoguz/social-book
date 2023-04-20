e867ee37c11b35db73cc3d7820718c880f777e27
tunahanoguz <tunahanog@gmail.com>
2023-04-20 14:44:48 +0300

    Create data seeder for Community entity

---
+++ Infrastructure/SocialBook.Persistence/DataSeeders/Communities/CommunitySeeder.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialBook.Domain.Entities.Communities;
+
+namespace SocialBook.Persistence.DataSeeders
+{
+    public static partial class DataSeeder
+    {
+        private static Community[] Communities => new Community[]
+        {
+            new Community { Id = new Guid(), Name = "Community 1", Description = "Community 1 description.", OwnerUser = Users[2] },
+            new Community { Id = new Guid(), Name = "Community 2", Description = "Community 1 description.", OwnerUser = Users[2] }
+        };
+
+        public static void SeedCommunities(this ModelBuilder builder)
+        {
+            builder.Entity<Community>().HasData(Communities);
+        }
+    }
+}
