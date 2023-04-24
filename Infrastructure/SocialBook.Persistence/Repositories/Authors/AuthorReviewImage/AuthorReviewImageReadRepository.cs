f26547dfd049f832749c4919643b47dea95d777c
tunahanoguz <tunahanog@gmail.com>
2023-04-24 11:54:40 +0300

    Customize AuthorReviewImage entity repository

--- Infrastructure/SocialBook.Persistence/Repositories/Authors/AuthorReviewImage/AuthorReviewImageReadRepository.cs
+++ Infrastructure/SocialBook.Persistence/Repositories/Authors/AuthorReviewImage/AuthorReviewImageReadRepository.cs
@@ -1,4 +1,5 @@
-﻿using SocialBook.Application.Repositories.Authors;
+﻿using Microsoft.EntityFrameworkCore;
+using SocialBook.Application.Repositories.Authors;
 using SocialBook.Domain.Entities.Authors;
 using SocialBook.Persistence.Contexts;
 using SocialBook.Persistence.Repositories.Common;
@@ -9,6 +10,16 @@ namespace SocialBook.Persistence.Repositories.Authors
     {
         public AuthorReviewImageReadRepository(SocialBookAPIDbContext context) : base(context)
         {
+        }
+
+        public async Task<List<AuthorReviewImage>> GetAuthorReviewImagesByFileExtensionAsync(string fileExtension)
+        {
+            return await GetWhere(a => a.FileExtension == fileExtension).ToListAsync();
+        }
+
+        public async Task<List<AuthorReviewImage>> GetAuthorReviewImagesByAuthorReviewAsync(Guid authorReviewId)
+        {
+            return await GetWhere(a => a.AuthorReviewId == authorReviewId).ToListAsync();
         }
     }
 }
