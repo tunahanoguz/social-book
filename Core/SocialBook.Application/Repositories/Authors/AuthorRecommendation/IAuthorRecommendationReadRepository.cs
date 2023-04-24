73f295f52535b62f27b3df6c5fd6717cf0e4ab89
tunahanoguz <tunahanog@gmail.com>
2023-04-24 11:41:46 +0300

    Customize AuthorRecommendation entity repository

--- Core/SocialBook.Application/Repositories/Authors/AuthorRecommendation/IAuthorRecommendationReadRepository.cs
+++ Core/SocialBook.Application/Repositories/Authors/AuthorRecommendation/IAuthorRecommendationReadRepository.cs
@@ -5,5 +5,8 @@ namespace SocialBook.Application.Repositories.Authors
 {
     public interface IAuthorRecommendationReadRepository : IReadRepository<AuthorRecommendation>
     {
+        Task<List<AuthorRecommendation>> GetAuthorRecommendationsByAuthorAsync(Guid authorId);
+        Task<List<AuthorRecommendation>> GetAuthorRecommendationsByRecommenderUserAsync(string recommenderUserId);
+        Task<List<AuthorRecommendation>> GetAuthorRecommendationsByRecipientUserAsync(string recipientUserId);
     }
 }
