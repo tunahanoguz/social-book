03f8e9cc207b96166bc8370484297ca32d89b1f5
tunahanoguz <tunahanog@gmail.com>
2023-04-24 17:00:57 +0300

    Create service for AuthorImage repository

---
+++ Core/SocialBook.Application/Services/Authors/IAuthorImageService.cs
@@ -0,0 +1,13 @@
+﻿using SocialBook.Domain.Entities.Authors;
+
+namespace SocialBook.Application.Services.Authors
+{
+    public interface IAuthorImageService
+    {
+        Task<List<AuthorImage>> GetAuthorImagesByFileExtensionAsync(string extension);
+        Task<List<AuthorImage>> GetAuthorImagesByAuthorAsync(Guid authorId);
+        Task<bool> CreateAuthorImageAsync(AuthorImage authorImage);
+        bool UpdateAuthorImage(AuthorImage authorImage);
+        bool DeleteAuthorImage(AuthorImage authorImage);
+    }
+}
---
+++ Infrastructure/SocialBook.Persistence/Services/Authors/AuthorImageService.cs
@@ -0,0 +1,53 @@
+﻿using SocialBook.Application.Repositories.Authors;
+using SocialBook.Application.Services.Authors;
+using SocialBook.Domain.Entities.Authors;
+
+namespace SocialBook.Persistence.Services.Authors
+{
+    public class AuthorImageService : IAuthorImageService
+    {
+        private readonly IAuthorImageReadRepository _authorImageReadRepository;
+        private readonly IAuthorImageWriteRepository _authorImageWriteRepository;
+
+        public AuthorImageService(IAuthorImageReadRepository authorImageReadRepository, IAuthorImageWriteRepository authorImageWriteRepository)
+        {
+            _authorImageReadRepository = authorImageReadRepository;
+            _authorImageWriteRepository = authorImageWriteRepository;
+        }
+
+        public async Task<List<AuthorImage>> GetAuthorImagesByFileExtensionAsync(string extension)
+        {
+            if (extension == null) { throw new ArgumentNullException(nameof(extension)); }
+
+            return await _authorImageReadRepository.GetAuthorImagesByFileExtensionAsync(extension);
+        }
+
+        public async Task<List<AuthorImage>> GetAuthorImagesByAuthorAsync(Guid authorId)
+        {
+            if (authorId == Guid.Empty) { throw new ArgumentNullException(nameof(authorId)); }
+
+            return await _authorImageReadRepository.GetAuthorImagesByAuthorAsync(authorId);
+        }
+
+        public async Task<bool> CreateAuthorImageAsync(AuthorImage authorImage)
+        {
+            if (authorImage == null) { throw new ArgumentNullException(nameof(authorImage)); }
+
+            return await _authorImageWriteRepository.AddAsync(authorImage);
+        }
+
+        public bool UpdateAuthorImage(AuthorImage authorImage)
+        {
+            if (authorImage == null) { throw new ArgumentNullException(nameof(authorImage)); }
+
+            return _authorImageWriteRepository.Update(authorImage);
+        }
+
+        public bool DeleteAuthorImage(AuthorImage authorImage)
+        {
+            if (authorImage == null) { throw new ArgumentNullException(nameof(authorImage)); }
+
+            return _authorImageWriteRepository.Remove(authorImage);
+        }
+    }
+}
