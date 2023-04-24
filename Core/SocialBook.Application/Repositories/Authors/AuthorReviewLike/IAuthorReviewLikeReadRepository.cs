c0b09f0f6bca74703af61106d05630ee46dd8fe9
tunahanoguz <tunahanog@gmail.com>
2023-04-24 11:51:34 +0300

    Customize AuthorReviewLike entity repository

--- Core/SocialBook.Application/Repositories/Authors/AuthorReviewLike/IAuthorReviewLikeReadRepository.cs
+++ Core/SocialBook.Application/Repositories/Authors/AuthorReviewLike/IAuthorReviewLikeReadRepository.cs
@@ -5,5 +5,7 @@ namespace SocialBook.Application.Repositories.Authors
 {
     public interface IAuthorReviewLikeReadRepository : IReadRepository<AuthorReviewLike>
     {
+        Task<List<AuthorReviewLike>> GetAuthorReviewLikesByAuthorReviewAsync(Guid authorReviewId);
+        Task<List<AuthorReviewLike>> GetAuthorReviewLikesByUserAsync(string userId);
     }
 }
