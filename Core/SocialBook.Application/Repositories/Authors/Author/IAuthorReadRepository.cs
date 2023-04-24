19cc2aa0b426865ec5bce7925c7cf583fd5d8b0d
tunahanoguz <tunahanog@gmail.com>
2023-04-24 11:16:59 +0300

    Customize Author entity repository

--- Core/SocialBook.Application/Repositories/Authors/Author/IAuthorReadRepository.cs
+++ Core/SocialBook.Application/Repositories/Authors/Author/IAuthorReadRepository.cs
@@ -5,5 +5,16 @@ namespace SocialBook.Application.Repositories.Authors
 {
     public interface IAuthorReadRepository : IReadRepository<Author>
     {
+        Task<List<Author>> GetAllAuthorsByFirstNameAsync(string firstName);
+        Task<List<Author>> GetAllAuthorsByLastNameAsync(string lastName);
+        Task<List<Author>> GetAllAuthorsByCountryOfBirthAsync(string country);
+        Task<List<Author>> GetAllAuthorsByYearOfBirthAsync(int year);
+        Task<List<Author>> GetAllAuthorsAllowedReviewAsync();
+        Task<List<Author>> GetAllAuthorsNotAllowedReviewAsync();
+        Task<List<Author>> GetAllAuthorsAllowedRecommendationAsync();
+        Task<List<Author>> GetAllAuthorsNotAllowedRecommendationAsync();
+        Task<List<Author>> GetAllAuthorsAllowedSubscriptionAsync();
+        Task<List<Author>> GetAllAuthorsNotAllowedSubscriptionAsync();
+        Task<List<Author>> GetAllAuthorsByCreatorUserAsync(string userId);
     }
 }
--- Infrastructure/SocialBook.Persistence/Repositories/Authors/Author/AuthorReadRepository.cs
+++ Infrastructure/SocialBook.Persistence/Repositories/Authors/Author/AuthorReadRepository.cs
@@ -1,4 +1,5 @@
-﻿using SocialBook.Application.Repositories.Authors;
+﻿using Microsoft.EntityFrameworkCore;
+using SocialBook.Application.Repositories.Authors;
 using SocialBook.Domain.Entities.Authors;
 using SocialBook.Persistence.Contexts;
 using SocialBook.Persistence.Repositories.Common;
@@ -9,6 +10,61 @@ namespace SocialBook.Persistence.Repositories.Authors
     {
         public AuthorReadRepository(SocialBookAPIDbContext context) : base(context)
         {
+        }
+
+        public async Task<List<Author>> GetAllAuthorsByFirstNameAsync(string firstName)
+        {
+            return await GetWhere(author => author.FirstName == firstName, false).ToListAsync();
+        }
+
+        public async Task<List<Author>> GetAllAuthorsByLastNameAsync(string lastName)
+        {
+            return await GetWhere(author => author.LastName == lastName, false).ToListAsync();
+        }
+
+        public async Task<List<Author>> GetAllAuthorsByCountryOfBirthAsync(string country)
+        {
+            return await GetWhere(author => author.CountryOfBirth == country, false).ToListAsync();
+        }
+
+        public async Task<List<Author>> GetAllAuthorsByYearOfBirthAsync(int year)
+        {
+            return await GetWhere(author => author.DateOfBirth.Year == year, false).ToListAsync();
+        }
+
+        public async Task<List<Author>> GetAllAuthorsAllowedReviewAsync()
+        {
+            return await GetWhere(author => author.IsAllowedReview == true, false).ToListAsync();
+        }
+
+        public async Task<List<Author>> GetAllAuthorsNotAllowedReviewAsync()
+        {
+            return await GetWhere(author => author.IsAllowedReview == false, false).ToListAsync();
+        }
+
+        public async Task<List<Author>> GetAllAuthorsAllowedRecommendationAsync()
+        {
+            return await GetWhere(author => author.IsAllowedRecommendation == true, false).ToListAsync();
+        }
+
+        public async Task<List<Author>> GetAllAuthorsNotAllowedRecommendationAsync()
+        {
+            return await GetWhere(author => author.IsAllowedRecommendation == false, false).ToListAsync();
+        }
+
+        public async Task<List<Author>> GetAllAuthorsAllowedSubscriptionAsync()
+        {
+            return await GetWhere(author => author.IsAllowedSubscription == true, false).ToListAsync();
+        }
+
+        public async Task<List<Author>> GetAllAuthorsNotAllowedSubscriptionAsync()
+        {
+            return await GetWhere(author => author.IsAllowedSubscription == false, false).ToListAsync();
+        }
+
+        public async Task<List<Author>> GetAllAuthorsByCreatorUserAsync(string userId)
+        {
+            return await GetWhere(author => author.CreatorUserId == userId, false).ToListAsync();
         }
     }
 }
