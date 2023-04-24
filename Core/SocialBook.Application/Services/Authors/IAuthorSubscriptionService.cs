d1f8bd28e508ec62808dfab5045b1ba2a60f6dab
tunahanoguz <tunahanog@gmail.com>
2023-04-24 16:20:24 +0300

    Create service for AuthorSubscription repository

---
+++ Core/SocialBook.Application/Services/Authors/IAuthorSubscriptionService.cs
@@ -0,0 +1,13 @@
+﻿using SocialBook.Domain.Entities.Authors;
+
+namespace SocialBook.Application.Services.Authors
+{
+    public interface IAuthorSubscriptionService
+    {
+        Task<List<AuthorSubscription>> GetAuthorSubscriptionsByAuthorAsync(Guid authorId);
+        Task<List<AuthorSubscription>> GetAuthorSubscriptionsByUserAsync(string userId);
+        Task<bool> CreateAuthorSubscriptionAsync(AuthorSubscription author);
+        bool UpdateAuthorSubscription(AuthorSubscription author);
+        bool DeleteAuthorSubscription(AuthorSubscription author);
+    }
+}
---
+++ Infrastructure/SocialBook.Persistence/Services/Authors/AuthorSubscriptionService.cs
@@ -0,0 +1,54 @@
+﻿using SocialBook.Application.Repositories.Authors;
+using SocialBook.Application.Services.Authors;
+using SocialBook.Domain.Entities.Authors;
+
+namespace SocialBook.Persistence.Services.Authors
+{
+    public class AuthorSubscriptionService : IAuthorSubscriptionService
+    {
+        private readonly IAuthorSubscriptionReadRepository _authorSubscriptionReadRepository;
+        private readonly IAuthorSubscriptionWriteRepository _authorSubscriptionWriteRepository;
+
+        public AuthorSubscriptionService(IAuthorSubscriptionReadRepository authorSubscriptionReadRepository,
+            IAuthorSubscriptionWriteRepository authorSubscriptionWriteRepository)
+        {
+            _authorSubscriptionReadRepository = authorSubscriptionReadRepository;
+            _authorSubscriptionWriteRepository = authorSubscriptionWriteRepository;
+        }
+
+        public async Task<List<AuthorSubscription>> GetAuthorSubscriptionsByAuthorAsync(Guid authorId)
+        {
+            if (authorId == Guid.Empty) { throw new ArgumentNullException(nameof(authorId)); }
+
+            return await _authorSubscriptionReadRepository.GetAuthorSubscriptionsByAuthorAsync(authorId);
+        }
+
+        public async Task<List<AuthorSubscription>> GetAuthorSubscriptionsByUserAsync(string userId)
+        {
+            if (userId == null) { throw new ArgumentNullException(nameof(userId)); }
+
+            return await _authorSubscriptionReadRepository.GetAuthorSubscriptionsByUserAsync(userId);
+        }
+
+        public async Task<bool> CreateAuthorSubscriptionAsync(AuthorSubscription author)
+        {
+            if (author == null) { throw new ArgumentNullException(nameof (author)); }
+
+            return await _authorSubscriptionWriteRepository.AddAsync(author);
+        }
+
+        public bool UpdateAuthorSubscription(AuthorSubscription author)
+        {
+            if (author == null) { throw new ArgumentNullException(nameof(author)); }
+
+            return _authorSubscriptionWriteRepository.Update(author);
+        }
+
+        public bool DeleteAuthorSubscription(AuthorSubscription author)
+        {
+            if (author == null) { throw new ArgumentNullException(nameof(author)); }
+
+            return _authorSubscriptionWriteRepository.Remove(author);
+        }
+    }
+}
