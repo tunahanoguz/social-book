22efd0e09721b08b6ee5ac9733378ba0f1a54850
tunahanoguz <tunahanog@gmail.com>
2023-04-24 16:49:17 +0300

    Create service for AuthorReviewDislike repository

---
+++ Core/SocialBook.Application/Services/Authors/IAuthorReviewDislikeService.cs
@@ -0,0 +1,13 @@
+﻿using SocialBook.Domain.Entities.Authors;
+
+namespace SocialBook.Application.Services.Authors
+{
+    public interface IAuthorReviewDislikeService
+    {
+        Task<List<AuthorReviewDislike>> GetAuthorReviewDislikesByAuthorReviewAsync(Guid authorReviewId);
+        Task<List<AuthorReviewDislike>> GetAuthorReviewDislikesByUserAsync(string userId);
+        Task<bool> CreateAuthorReviewDislikeAsync(AuthorReviewDislike authorReviewDislike);
+        bool UpdateAuthorReviewDislike(AuthorReviewDislike authorReviewDislike);
+        bool DeleteAuthorReviewDislike(AuthorReviewDislike authorReviewDislike);
+    }
+}
---
+++ Infrastructure/SocialBook.Persistence/Services/Authors/AuthorReviewDislikeService.cs
@@ -0,0 +1,54 @@
+﻿using SocialBook.Application.Repositories.Authors;
+using SocialBook.Application.Services.Authors;
+using SocialBook.Domain.Entities.Authors;
+
+namespace SocialBook.Persistence.Services.Authors
+{
+    public class AuthorReviewDislikeService : IAuthorReviewDislikeService
+    {
+        private readonly IAuthorReviewDislikeReadRepository _authorReviewDislikeReadRepository;
+        private readonly IAuthorReviewDislikeWriteRepository _authorReviewDislikeWriteRepository;
+
+        public AuthorReviewDislikeService(IAuthorReviewDislikeReadRepository authorReviewDislikeReadRepository,
+            IAuthorReviewDislikeWriteRepository authorReviewDislikeWriteRepository)
+        {
+            _authorReviewDislikeReadRepository = authorReviewDislikeReadRepository;
+            _authorReviewDislikeWriteRepository = authorReviewDislikeWriteRepository;
+        }
+
+        public async Task<List<AuthorReviewDislike>> GetAuthorReviewDislikesByAuthorReviewAsync(Guid authorReviewId)
+        {
+            if (authorReviewId == Guid.Empty) { throw new ArgumentException(nameof(authorReviewId)); }
+
+            return await _authorReviewDislikeReadRepository.GetAuthorReviewDislikesByAuthorReviewAsync(authorReviewId);
+        }
+
+        public async Task<List<AuthorReviewDislike>> GetAuthorReviewDislikesByUserAsync(string userId)
+        {
+            if (userId == null) { throw new ArgumentNullException(nameof(userId)); }
+
+            return await _authorReviewDislikeReadRepository.GetAuthorReviewDislikesByUserAsync(userId);
+        }
+
+        public async Task<bool> CreateAuthorReviewDislikeAsync(AuthorReviewDislike authorReviewDislike)
+        {
+            if (authorReviewDislike == null) { throw new ArgumentNullException(nameof(authorReviewDislike)); }
+
+            return await _authorReviewDislikeWriteRepository.AddAsync(authorReviewDislike);
+        }
+
+        public bool UpdateAuthorReviewDislike(AuthorReviewDislike authorReviewDislike)
+        {
+            if (authorReviewDislike == null) { throw new ArgumentNullException(nameof(authorReviewDislike)); }
+
+            return _authorReviewDislikeWriteRepository.Update(authorReviewDislike);
+        }
+
+        public bool DeleteAuthorReviewDislike(AuthorReviewDislike authorReviewDislike)
+        {
+            if (authorReviewDislike == null) { throw new ArgumentNullException(nameof(authorReviewDislike)); }
+
+            return _authorReviewDislikeWriteRepository.Remove(authorReviewDislike);
+        }
+    }
+}
