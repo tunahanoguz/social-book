c66bcdb94d5f93b424958945960b46dc155b4765
tunahanoguz <tunahanog@gmail.com>
2023-04-25 15:00:07 +0300

    Create summaries for repository and service methods for AuthorReviewImage

--- Core/SocialBook.Application/Services/Authors/IAuthorReviewImageService.cs
+++ Core/SocialBook.Application/Services/Authors/IAuthorReviewImageService.cs
@@ -4,10 +4,48 @@ namespace SocialBook.Application.Services.Authors
 {
     public interface IAuthorReviewImageService
     {
-        Task<List<AuthorReviewImage>> GetAuthorReviewImagesByFileExtensionAsync(string extension);
+        /// <summary>
+        /// Get all author review images with the file extension provided as a parameter
+        /// </summary>
+        /// <param name="fileExtension">The file extension</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains the author review images with the file extension provided as a parameter
+        /// </returns>
+        Task<List<AuthorReviewImage>> GetAuthorReviewImagesByFileExtensionAsync(string fileExtension);
+
+        /// <summary>
+        /// Get all author review images belonging to the author review whose ID is provided as a parameter
+        /// </summary>
+        /// <param name="authorReviewId">The author review identifier</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains the author review images belonging to the author review whose ID is provided as a parameter
+        /// </returns>
         Task<List<AuthorReviewImage>> GetAuthorReviewImagesByAuthorReviewAsync(Guid authorReviewId);
+
+        /// <summary>
+        /// Create a new author review image
+        /// </summary>
+        /// <param name="authorReviewImage">The author review image entity</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains a boolean value indicating whether the author review image was created successfully or not
+        /// </returns>
         Task<bool> CreateAuthorReviewImageAsync(AuthorReviewImage authorReviewImage);
+
+        /// <summary>
+        /// Update the author review image provided as a parameter
+        /// </summary>
+        /// <param name="authorReviewImage">The author review image entity</param>
+        /// <returns>A boolean value indicating whether the author review image was updated successfully or not</returns>
         bool UpdateAuthorReviewImage(AuthorReviewImage authorReviewImage);
+
+        /// <summary>
+        /// Delete the author review image provided as a parameter
+        /// </summary>
+        /// <param name="authorReviewImage">The author review image entity</param>
+        /// <returns>A boolean value indicating whether the author review image was deleted successfully or not</returns>
         bool DeleteAuthorReviewImage(AuthorReviewImage authorReviewImage);
     }
 }
--- Infrastructure/SocialBook.Persistence/Services/Authors/AuthorReviewImageService.cs
+++ Infrastructure/SocialBook.Persistence/Services/Authors/AuthorReviewImageService.cs
@@ -9,19 +9,36 @@ namespace SocialBook.Persistence.Services.Authors
         private readonly IAuthorReviewImageReadRepository _authorReviewImageReadRepository;
         private readonly IAuthorReviewImageWriteRepository _authorReviewImageWriteRepository;
 
-        public AuthorReviewImageService(IAuthorReviewImageReadRepository authorReviewImageReadRepository, IAuthorReviewImageWriteRepository authorReviewImageWriteRepository)
+        public AuthorReviewImageService(IAuthorReviewImageReadRepository authorReviewImageReadRepository,
+            IAuthorReviewImageWriteRepository authorReviewImageWriteRepository)
         {
             _authorReviewImageReadRepository = authorReviewImageReadRepository;
             _authorReviewImageWriteRepository = authorReviewImageWriteRepository;
         }
 
-        public async Task<List<AuthorReviewImage>> GetAuthorReviewImagesByFileExtensionAsync(string extension)
+        /// <summary>
+        /// Get all author review images with the file extension provided as a parameter
+        /// </summary>
+        /// <param name="fileExtension">The file extension</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains the author review images with the file extension provided as a parameter
+        /// </returns>
+        public async Task<List<AuthorReviewImage>> GetAuthorReviewImagesByFileExtensionAsync(string extenfileExtensionsion)
         {
             if (extension == null) { throw new ArgumentNullException(nameof(extension)); }
 
             return await _authorReviewImageReadRepository.GetAuthorReviewImagesByFileExtensionAsync(extension);
         }
 
+        /// <summary>
+        /// Get all author review images belonging to the author review whose ID is provided as a parameter
+        /// </summary>
+        /// <param name="authorReviewId">The author review identifier</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains the author review images belonging to the author review whose ID is provided as a parameter
+        /// </returns>
         public async Task<List<AuthorReviewImage>> GetAuthorReviewImagesByAuthorReviewAsync(Guid authorReviewId)
         {
             if (authorReviewId == Guid.Empty) { throw new ArgumentNullException(nameof(authorReviewId)); }
@@ -29,6 +46,14 @@ namespace SocialBook.Persistence.Services.Authors
             return await _authorReviewImageReadRepository.GetAuthorReviewImagesByAuthorReviewAsync(authorReviewId);
         }
 
+        /// <summary>
+        /// Create a new author review image
+        /// </summary>
+        /// <param name="authorReviewImage">The author review image entity</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains a boolean value indicating whether the author review image was created successfully or not
+        /// </returns>
         public async Task<bool> CreateAuthorReviewImageAsync(AuthorReviewImage authorReviewImage)
         {
             if (_authorReviewImageReadRepository == null) { throw new ArgumentNullException(nameof(authorReviewImage)); };
@@ -36,6 +61,11 @@ namespace SocialBook.Persistence.Services.Authors
             return await _authorReviewImageWriteRepository.AddAsync(authorReviewImage);
         }
 
+        /// <summary>
+        /// Update the author review image provided as a parameter
+        /// </summary>
+        /// <param name="authorReviewImage">The author review image entity</param>
+        /// <returns>A boolean value indicating whether the author review image was updated successfully or not</returns>
         public bool UpdateAuthorReviewImage(AuthorReviewImage authorReviewImage)
         {
             if (_authorReviewImageReadRepository == null) { throw new ArgumentNullException(nameof(authorReviewImage)); };
@@ -43,6 +73,11 @@ namespace SocialBook.Persistence.Services.Authors
             return _authorReviewImageWriteRepository.Update(authorReviewImage);
         }
 
+        /// <summary>
+        /// Delete the author review image provided as a parameter
+        /// </summary>
+        /// <param name="authorReviewImage">The author review image entity</param>
+        /// <returns>A boolean value indicating whether the author review image was deleted successfully or not</returns>
         public bool DeleteAuthorReviewImage(AuthorReviewImage authorReviewImage)
         {
             if (_authorReviewImageReadRepository == null) { throw new ArgumentNullException(nameof(authorReviewImage)); };
