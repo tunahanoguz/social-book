d7ab3c3729daab612dcbe8e6622c79eca5237715
tunahanoguz <tunahanog@gmail.com>
2023-04-25 12:16:31 +0300

    Create summaries for repository and service methods of AuthorReview

--- Core/SocialBook.Application/Services/Authors/IAuthorReviewService.cs
+++ Core/SocialBook.Application/Services/Authors/IAuthorReviewService.cs
@@ -4,11 +4,58 @@ namespace SocialBook.Application.Services.Authors
 {
     public interface IAuthorReviewService
     {
+        /// <summary>
+        /// Get all author reviews matching the rating value provided as a parameter
+        /// </summary>
+        /// <param name="rating">The author review rating</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains the author reviews matching the rating value provided as a parameter
+        /// </returns>
         Task<List<AuthorReview>> GetAuthorReviewsByRatingAsync(int rating);
+
+        /// <summary>
+        /// Get author reviews belonging to the author whose ID is provided as a parameter
+        /// </summary>
+        /// <param name="authorId">The author identifier</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains the author reviews belonging to the author whose ID is provided as a parameter
+        /// </returns>
         Task<List<AuthorReview>> GetAuthorReviewsByAuthorAsync(Guid authorId);
+
+        /// <summary>
+        /// Get author reviews belonging to the user whose ID is provided as a parameter
+        /// </summary>
+        /// <param name="userId">The user identifier</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains the author reviews belonging to the user whose ID is provided as a parameter
+        /// </returns>
         Task<List<AuthorReview>> GetAuthorReviewsByUserAsync(string userId);
+
+        /// <summary>
+        /// Create a new author review
+        /// </summary>
+        /// <param name="authorReview">The author review entity</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains a boolean value indicating whether the author review was created successfully or not
+        /// </returns>
         Task<bool> CreateAuthorReviewAsync(AuthorReview authorReview);
+
+        /// <summary>
+        /// Update the author review provided as a parameter
+        /// </summary>
+        /// <param name="authorReview">The author review entity</param>
+        /// <returns>A boolean value indicating whether the author review was updated successfully or not</returns>
         bool UpdateAuthorReview(AuthorReview authorReview);
+
+        /// <summary>
+        /// Delete the author review provided as a parameter
+        /// </summary>
+        /// <param name="authorReview">The author review entity</param>
+        /// <returns>A boolean value indicating whether the author review was deleted successfully or not</returns>
         bool DeleteAuthorReview(AuthorReview authorReview);
     }
 }
--- Infrastructure/SocialBook.Persistence/Repositories/Authors/AuthorReview/AuthorReviewReadRepository.cs
+++ Infrastructure/SocialBook.Persistence/Repositories/Authors/AuthorReview/AuthorReviewReadRepository.cs
@@ -12,16 +12,40 @@ namespace SocialBook.Persistence.Repositories.Authors
         {
         }
 
+        /// <summary>
+        /// Get all author reviews matching the rating value provided as a parameter
+        /// </summary>
+        /// <param name="rating">The author review rating</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains the author reviews matching the rating value provided as a parameter
+        /// </returns>
         public async Task<List<AuthorReview>> GetAuthorReviewsByRatingAsync(int rating)
         {
             return await GetWhere(a => a.Rating == rating).ToListAsync();
         }
 
+        /// <summary>
+        /// Get author reviews belonging to the author whose ID is provided as a parameter
+        /// </summary>
+        /// <param name="authorId">The author identifier</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains the author reviews belonging to the author whose ID is provided as a parameter
+        /// </returns>
         public async Task<List<AuthorReview>> GetAuthorReviewsByAuthorAsync(Guid authorId)
         {
             return await GetWhere(a => a.AuthorId == authorId).ToListAsync();
         }
 
+        /// <summary>
+        /// Get author reviews belonging to the user whose ID is provided as a parameter
+        /// </summary>
+        /// <param name="userId">The user identifier</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains the author reviews belonging to the user whose ID is provided as a parameter
+        /// </returns>
         public async Task<List<AuthorReview>> GetAuthorReviewsByUserAsync(string userId)
         {
             return await GetWhere(a => a.UserId == userId).ToListAsync();
