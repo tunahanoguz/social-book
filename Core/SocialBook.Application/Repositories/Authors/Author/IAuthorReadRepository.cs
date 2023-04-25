451276620faa391185a2ab78e7f6c391085be8a0
tunahanoguz <tunahanog@gmail.com>
2023-04-25 11:25:54 +0300

    Create summaries for Author repository methods

--- Core/SocialBook.Application/Repositories/Authors/Author/IAuthorReadRepository.cs
+++ Core/SocialBook.Application/Repositories/Authors/Author/IAuthorReadRepository.cs
@@ -5,16 +5,108 @@ namespace SocialBook.Application.Repositories.Authors
 {
     public interface IAuthorReadRepository : IReadRepository<Author>
     {
+        /// <summary>
+        /// Get all authors with the first name provided as a parameter
+        /// </summary>
+        /// <param name="firstName">The first name of the author</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains the authors with the first name provided as a parameter
+        /// </returns>
         Task<List<Author>> GetAuthorsByFirstNameAsync(string firstName);
+
+        /// <summary>
+        /// Get all authors with the last name provided as a parameter
+        /// </summary>
+        /// <param name="lastName">The last name of the author</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains the authors with the last name provided as a parameter
+        /// </returns>
         Task<List<Author>> GetAuthorsByLastNameAsync(string lastName);
+
+        /// <summary>
+        /// Get all authors born in the country provided as a parameter
+        /// </summary>
+        /// <param name="country">The country where the author was born</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains the authors born in the country provided as a parameter
+        /// </returns>
         Task<List<Author>> GetAuthorsByCountryOfBirthAsync(string country);
+
+        /// <summary>
+        /// Get all authors born in the year provided as a parameter
+        /// </summary>
+        /// <param name="year">The year where the author was born</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains the authors born in the year provided as a parameter
+        /// </returns>
         Task<List<Author>> GetAuthorsByYearOfBirthAsync(int year);
+
+        /// <summary>
+        /// Get all authors allowed to have reviews published about them
+        /// </summary>
+        /// <returns>
+        /// /// A task that represents the asynchronous operation
+        /// The task result contains the authors allowed to have reviews published about them
+        /// </returns>
         Task<List<Author>> GetAuthorsAllowedReviewAsync();
+
+        /// <summary>
+        /// Get all authors not allowed to have reviews published about them
+        /// </summary>
+        /// <returns>
+        /// /// A task that represents the asynchronous operation
+        /// The task result contains the authors not allowed to have reviews published about them
+        /// </returns>
         Task<List<Author>> GetAuthorsNotAllowedReviewAsync();
+
+        /// <summary>
+        /// Get all authors allowed to be recommended by users to other users
+        /// </summary>
+        /// <returns>
+        /// /// A task that represents the asynchronous operation
+        /// The task result contains the authors allowed to be recommended by users to other users
+        /// </returns>
         Task<List<Author>> GetAuthorsAllowedRecommendationAsync();
+
+        /// <summary>
+        /// Get all authors not allowed to be recommended by users to other users
+        /// </summary>
+        /// <returns>
+        /// /// A task that represents the asynchronous operation
+        /// The task result contains the authors not allowed to be recommended by users to other users
+        /// </returns>
         Task<List<Author>> GetAuthorsNotAllowedRecommendationAsync();
+
+        /// <summary>
+        /// Get all authors who are allowed to be subscribed to by users
+        /// </summary>
+        /// <returns>
+        /// /// A task that represents the asynchronous operation
+        /// The task result contains the authors who are allowed to be subscribed to by users
+        /// </returns>
         Task<List<Author>> GetAuthorsAllowedSubscriptionAsync();
+
+        /// <summary>
+        /// Get all authors who are not allowed to be subscribed to by users
+        /// </summary>
+        /// <returns>
+        /// /// A task that represents the asynchronous operation
+        /// The task result contains the authors who are not allowed to be subscribed to by users
+        /// </returns>
         Task<List<Author>> GetAuthorsNotAllowedSubscriptionAsync();
+
+        /// <summary>
+        /// Get all authors belonging to the user whose ID is provided as a parameter
+        /// </summary>
+        /// <param name="userId">The creator user identifier</param>
+        /// <returns>
+        /// /// A task that represents the asynchronous operation
+        /// The task result contains the authors belonging to the user whose ID is provided as a parameter
+        /// </returns>
         Task<List<Author>> GetAuthorsByCreatorUserAsync(string userId);
     }
 }
--- Infrastructure/SocialBook.Persistence/Repositories/Authors/Author/AuthorReadRepository.cs
+++ Infrastructure/SocialBook.Persistence/Repositories/Authors/Author/AuthorReadRepository.cs
@@ -12,56 +12,138 @@ namespace SocialBook.Persistence.Repositories.Authors
         {
         }
 
+        /// <summary>
+        /// Get all authors with the first name provided as a parameter
+        /// </summary>
+        /// <param name="firstName">The first name of the author</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains the authors with the first name provided as a parameter
+        /// </returns>
         public async Task<List<Author>> GetAuthorsByFirstNameAsync(string firstName)
         {
             return await GetWhere(author => author.FirstName == firstName, false).ToListAsync();
         }
 
+        /// <summary>
+        /// Get all authors with the last name provided as a parameter
+        /// </summary>
+        /// <param name="lastName">The last name of the author</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains the authors with the last name provided as a parameter
+        /// </returns>
         public async Task<List<Author>> GetAuthorsByLastNameAsync(string lastName)
         {
             return await GetWhere(author => author.LastName == lastName, false).ToListAsync();
         }
 
+        /// <summary>
+        /// Get all authors born in the country provided as a parameter
+        /// </summary>
+        /// <param name="country">The country where the author was born</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains the authors born in the country provided as a parameter
+        /// </returns>
         public async Task<List<Author>> GetAuthorsByCountryOfBirthAsync(string country)
         {
             return await GetWhere(author => author.CountryOfBirth == country, false).ToListAsync();
         }
 
+        /// <summary>
+        /// Get all authors born in the year provided as a parameter
+        /// </summary>
+        /// <param name="year">The year where the author was born</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains the authors born in the year provided as a parameter
+        /// </returns>
         public async Task<List<Author>> GetAuthorsByYearOfBirthAsync(int year)
         {
             return await GetWhere(author => author.DateOfBirth.Year == year, false).ToListAsync();
         }
 
+        /// <summary>
+        /// Get all authors allowed to have reviews published about them
+        /// </summary>
+        /// <returns>
+        /// /// A task that represents the asynchronous operation
+        /// The task result contains the authors allowed to have reviews published about them
+        /// </returns>
         public async Task<List<Author>> GetAuthorsAllowedReviewAsync()
         {
             return await GetWhere(author => author.IsAllowedReview == true, false).ToListAsync();
         }
 
+        /// <summary>
+        /// Get all authors not allowed to have reviews published about them
+        /// </summary>
+        /// <returns>
+        /// /// A task that represents the asynchronous operation
+        /// The task result contains the authors not allowed to have reviews published about them
+        /// </returns>
         public async Task<List<Author>> GetAuthorsNotAllowedReviewAsync()
         {
             return await GetWhere(author => author.IsAllowedReview == false, false).ToListAsync();
         }
 
+        /// <summary>
+        /// Get all authors allowed to be recommended by users to other users
+        /// </summary>
+        /// <returns>
+        /// /// A task that represents the asynchronous operation
+        /// The task result contains the authors allowed to be recommended by users to other users
+        /// </returns>
         public async Task<List<Author>> GetAuthorsAllowedRecommendationAsync()
         {
             return await GetWhere(author => author.IsAllowedRecommendation == true, false).ToListAsync();
         }
 
+        /// <summary>
+        /// Get all authors not allowed to be recommended by users to other users
+        /// </summary>
+        /// <returns>
+        /// /// A task that represents the asynchronous operation
+        /// The task result contains the authors not allowed to be recommended by users to other users
+        /// </returns>
         public async Task<List<Author>> GetAuthorsNotAllowedRecommendationAsync()
         {
             return await GetWhere(author => author.IsAllowedRecommendation == false, false).ToListAsync();
         }
 
+        /// <summary>
+        /// Get all authors who are allowed to be subscribed to by users
+        /// </summary>
+        /// <returns>
+        /// /// A task that represents the asynchronous operation
+        /// The task result contains the authors who are allowed to be subscribed to by users
+        /// </returns>
         public async Task<List<Author>> GetAuthorsAllowedSubscriptionAsync()
         {
             return await GetWhere(author => author.IsAllowedSubscription == true, false).ToListAsync();
         }
 
+        /// <summary>
+        /// Get all authors who are not allowed to be subscribed to by users
+        /// </summary>
+        /// <returns>
+        /// /// A task that represents the asynchronous operation
+        /// The task result contains the authors who are not allowed to be subscribed to by users
+        /// </returns>
         public async Task<List<Author>> GetAuthorsNotAllowedSubscriptionAsync()
         {
             return await GetWhere(author => author.IsAllowedSubscription == false, false).ToListAsync();
         }
 
+        /// <summary>
+        /// Get all authors belonging to the user whose ID is provided as a parameter
+        /// </summary>
+        /// <param name="userId">The creator user identifier</param>
+        /// <returns>
+        /// /// A task that represents the asynchronous operation
+        /// The task result contains the authors belonging to the user whose ID is provided as a parameter
+        /// </returns>
         public async Task<List<Author>> GetAuthorsByCreatorUserAsync(string userId)
         {
             return await GetWhere(author => author.CreatorUserId == userId, false).ToListAsync();
