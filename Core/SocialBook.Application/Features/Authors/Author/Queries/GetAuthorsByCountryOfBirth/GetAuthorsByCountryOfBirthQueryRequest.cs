4608baf1fe037c866ba4349f03263548d0b24484
tunahanoguz <tunahanog@gmail.com>
2023-04-26 10:19:17 +0300

    Fix inaccurate property summaries

--- Core/SocialBook.Application/Features/Authors/Author/Queries/GetAuthorsByCountryOfBirth/GetAuthorsByCountryOfBirthQueryRequest.cs
+++ Core/SocialBook.Application/Features/Authors/Author/Queries/GetAuthorsByCountryOfBirth/GetAuthorsByCountryOfBirthQueryRequest.cs
@@ -5,7 +5,7 @@ namespace SocialBook.Application.Features.Authors
     public class GetAuthorsByCountryOfBirthQueryRequest : IRequest<List<GetAuthorsByCountryOfBirthQueryResponse>>
     {
         /// <summary>
-        /// Gets or sets the first name
+        /// Gets or sets the country of birth of the author
         /// </summary>
         public string CountryOfBirth { get; set; }
     }
--- Core/SocialBook.Application/Features/Authors/Author/Queries/GetAuthorsByLastName/GetAuthorsByLastNameQueryRequest.cs
+++ Core/SocialBook.Application/Features/Authors/Author/Queries/GetAuthorsByLastName/GetAuthorsByLastNameQueryRequest.cs
@@ -5,7 +5,7 @@ namespace SocialBook.Application.Features.Authors
     public class GetAuthorsByLastNameQueryRequest : IRequest<List<GetAuthorsByLastNameQueryResponse>>
     {
         /// <summary>
-        /// Gets or sets the first name
+        /// Gets or sets the last name
         /// </summary>
         public string LastName { get; set; }
     }
