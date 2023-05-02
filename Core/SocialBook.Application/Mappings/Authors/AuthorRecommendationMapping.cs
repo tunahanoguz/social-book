8e16d269c9e6a7d591cddf1cef02f3a668713707
tunahanoguz <tunahanog@gmail.com>
2023-05-02 16:18:48 +0300

    Create author recommendation mapping

---
+++ Core/SocialBook.Application/Mappings/Authors/AuthorRecommendationMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using SocialBook.Application.DTOs.Authors.AuthorRecommendation;
+using SocialBook.Domain.Entities.Authors;
+
+namespace SocialBook.Application.Mappings.Authors
+{
+    public class AuthorRecommendationMapping : Profile
+    {
+        public AuthorRecommendationMapping()
+        {
+            CreateMap<AuthorRecommendation, AuthorRecommendationDto>();
+        }
+    }
+}
