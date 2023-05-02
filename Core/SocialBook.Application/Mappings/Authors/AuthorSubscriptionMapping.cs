547b7952a229b5181fabcdef7f12937da268b284
tunahanoguz <tunahanog@gmail.com>
2023-05-02 17:45:36 +0300

    Create dto for author subscription

---
+++ Core/SocialBook.Application/Mappings/Authors/AuthorSubscriptionMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using SocialBook.Application.DTOs.Authors.AuthorSubscription;
+using SocialBook.Domain.Entities.Authors;
+
+namespace SocialBook.Application.Mappings.Authors
+{
+    public class AuthorSubscriptionMapping : Profile
+    {
+        public AuthorSubscriptionMapping()
+        {
+            CreateMap<AuthorSubscription, AuthorSubscriptionDto>().ReverseMap();
+        }
+    }
+}
