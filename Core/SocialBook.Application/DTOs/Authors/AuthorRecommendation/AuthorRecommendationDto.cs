a143fe1e6d9d69b722a05b70be1281c64d006518
tunahanoguz <tunahanog@gmail.com>
2023-05-02 16:17:33 +0300

    Create dto for author recommendation

---
+++ Core/SocialBook.Application/DTOs/Authors/AuthorRecommendation/AuthorRecommendationDto.cs
@@ -0,0 +1,20 @@
+﻿namespace SocialBook.Application.DTOs.Authors.AuthorRecommendation
+{
+    public class AuthorRecommendationDto
+    {
+        /// <summary>
+        /// Gets or sets the recommended author identifier
+        /// </summary>
+        public Guid AuthorId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the recommender user identifier
+        /// </summary>
+        public string RecommenderUserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the recommendation recipient user identifier
+        /// </summary>
+        public string RecipientUserId { get; set; }
+    }
+}
