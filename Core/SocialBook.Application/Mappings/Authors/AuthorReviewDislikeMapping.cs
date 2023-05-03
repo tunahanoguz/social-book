ca8a18161e7c0fe5b18133ed96dd2f3dfaa7e5cb
tunahanoguz <tunahanog@gmail.com>
2023-05-03 20:07:52 +0300

    Create dtos and mappings for author review like-dislike

---
+++ Core/SocialBook.Application/Mappings/Authors/AuthorReviewDislikeMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using SocialBook.Application.DTOs.Authors.AuthorReviewDislike;
+using SocialBook.Domain.Entities.Authors;
+
+namespace SocialBook.Application.Mappings.Authors
+{
+    public class AuthorReviewDislikeMapping : Profile
+    {
+        public AuthorReviewDislikeMapping()
+        {
+            CreateMap<AuthorReviewDislike, AuthorReviewDislikeDto>().ReverseMap();
+        }
+    }
+}
---
+++ Core/SocialBook.Application/Mappings/Authors/AuthorReviewLikeMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using SocialBook.Application.DTOs.Authors.AuthorReviewLike;
+using SocialBook.Domain.Entities.Authors;
+
+namespace SocialBook.Application.Mappings.Authors
+{
+    public class AuthorReviewLikeMapping : Profile
+    {
+        public AuthorReviewLikeMapping()
+        {
+            CreateMap<AuthorReviewLike, AuthorReviewLikeDto>().ReverseMap();
+        }
+    }
+}
