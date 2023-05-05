e3fea6023a76c804973ca97a7f824a71cfb3e123
tunahanoguz <tunahanog@gmail.com>
2023-05-05 13:33:36 +0300

    Create dto for author image

---
+++ Core/SocialBook.Application/DTOs/Authors/AuthorImage/AuthorImageDto.cs
@@ -0,0 +1,30 @@
+﻿namespace SocialBook.Application.DTOs.Authors.AuthorImage
+{
+    public class AuthorImageDto
+    {
+        /// <summary>
+        /// The author image identifier
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// The image file name
+        /// </summary>
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// The image file extension
+        /// </summary>
+        public string FileExtension { get; set; }
+
+        /// <summary>
+        /// The image file size
+        /// </summary>
+        public double FileSize { get; set; }
+
+        /// <summary>
+        /// The author identifier
+        /// </summary>
+        public Guid AuthorId { get; set; }
+    }
+}
