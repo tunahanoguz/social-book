dc8ca7af2ce2638f9bacaec15df415fe43d1f1cb
tunahanoguz <tunahanog@gmail.com>
2023-05-06 15:29:35 +0300

    Create dto and mappings for author review image

---
+++ Core/SocialBook.Application/DTOs/Authors/AuthorReviewImage/AuthorReviewImageDto.cs
@@ -0,0 +1,30 @@
+﻿namespace SocialBook.Application.DTOs.Authors.AuthorReviewImage
+{
+    public class AuthorReviewImageDto
+    {
+        /// <summary>
+        /// The author review image identifier
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// The image file name
+        /// </summary>
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// The image file extension
+        /// </summary>
+        public string FileExtension { get; set; }
+
+        /// <summary>
+        /// The image file size
+        /// </summary>
+        public double FileSize { get; set; }
+
+        /// <summary>
+        /// The associated author review identifier
+        /// </summary>
+        public Guid AuthorReviewId { get; set; }
+    }
+}
---
+++ Core/SocialBook.Application/Mappings/Authors/AuthorReviewImageMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using SocialBook.Application.DTOs.Authors.AuthorReviewImage;
+using SocialBook.Domain.Entities.Authors;
+
+namespace SocialBook.Application.Mappings.Authors
+{
+    public class AuthorReviewImageMapping : Profile
+    {
+        public AuthorReviewImageMapping()
+        {
+            CreateMap<AuthorReviewImage, AuthorReviewImageDto>().ReverseMap();
+        }
+    }
+}
