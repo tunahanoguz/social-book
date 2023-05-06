57c49d37543d4759ac11bc0c27cccb12644804a2
tunahanoguz <tunahanog@gmail.com>
2023-05-06 16:08:04 +0300

    Create validators for author images

---
+++ Core/SocialBook.Application/Validators/Authors/AuthorImage/CreateAuthorImageCommandRequestValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using SocialBook.Application.Features.Commands;
+
+namespace SocialBook.Application.Validators.Authors.AuthorImage
+{
+    public class CreateAuthorImageCommandRequestValidator : AbstractValidator<CreateAuthorImageCommandRequest>
+    {
+        public CreateAuthorImageCommandRequestValidator()
+        {
+            RuleFor(x => x.AuthorId)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("The author identifier cannot be null or empty!");
+
+            RuleFor(x => x.AuthorId)
+                .Must(authorId => IsValidGuid(authorId.ToString()))
+                .WithMessage("The author identifier must be a valid GUID!");
+        }
+
+        private bool IsValidGuid(string id)
+        {
+            Guid guid;
+            return Guid.TryParse(id, out guid);
+        }
+    }
+}
