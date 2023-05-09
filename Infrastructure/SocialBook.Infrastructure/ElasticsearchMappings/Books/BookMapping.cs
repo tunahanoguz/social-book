e2baada651558a3764128bebd8907e1c165ee613
tunahanoguz <tunahanog@gmail.com>
2023-05-09 19:35:47 +0300

    Create elasticsearch mapping for book

---
+++ Infrastructure/SocialBook.Infrastructure/ElasticsearchMappings/Books/BookMapping.cs
@@ -0,0 +1,28 @@
+﻿using Nest;
+using SocialBook.Domain.Entities.Books;
+
+namespace SocialBook.Infrastructure.ElasticsearchMappings
+{
+    public static partial class ElasticsearchMapping
+    {
+        public static CreateIndexDescriptor BookMapping(this CreateIndexDescriptor descriptor)
+        {
+            return descriptor.Map<Book>(m => m.Properties(p => p
+                .Keyword(k => k.Name(n => n.Id))
+                .Text(t => t.Name(n => n.Isbn))
+                .Text(t => t.Name(n => n.Description))
+                .Number(n => n.Name(n => n.PublishedYear))
+                .Number(n => n.Name(n => n.PageCount))
+                .Text(t => t.Name(n => n.Language))
+                .Text(t => t.Name(n => n.MetaTitle))
+                .Text(t => t.Name(n => n.MetaDescription))
+                .Text(t => t.Name(n => n.MetaKeywords))
+                .Text(t => t.Name(n => n.Slug))
+                .Boolean(t => t.Name(n => n.IsAllowedReview))
+                .Boolean(t => t.Name(n => n.IsAllowedRecommendation))
+                .Boolean(t => t.Name(n => n.IsAllowedSubscription))
+                .Text(t => t.Name(n => n.CreatorUserId))
+            ));
+        }
+    }
+}
